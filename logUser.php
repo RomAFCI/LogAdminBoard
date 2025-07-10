c3b3647db9e447307a05fedcca8b8ec5a2ea7f2c
RomAFCI <adnot.rom@gmail.com>
2025-07-10 11:24:03 +0200

    save

--- logUser.php
+++ logUser.php
@@ -1,8 +1,9 @@
 <?php
-$host = 'db';
+session_start();
+$host = 'localhost';
 $dbname = 'bddAdminBoard';
 $user = 'root';
-$password = 'root';
+$password = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
@@ -15,3 +16,73 @@ try {
     echo "Erreur de connexion : " . $e->getMessage();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+if(!isset($_SESSION['user'])){
+    echo '<form method="POST">
+        <label>Identifiant</label>
+        <input type="text" name="identifiant">
+        <label>Password</label>
+        <input type="password" name="password">
+        <input type="submit" name="submitConnection" value="Se connecter">
+
+    </form>';
+}
+else{
+echo'<form method="POST">
+    <input type="submit" name="deconnexion" value="Deconnexion">
+</form>';
+
+echo "Bonjour, " . $_SESSION['user']['nomUser'] . " " . $_SESSION['user']['prenomUser'] . ". Vous êtes connecté.";
+}
+
+?>
+
+ 
+    <?php
+    if (isset($_POST['submitConnection'])) {
+        $id = $_POST['identifiant'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT * FROM `users` WHERE adresseMailUser = '$id'";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        var_dump($results);
+
+        if ($password == $results[0]['passwordUser']) {
+            $_SESSION['user'] = [
+                "idUser" => $results[0]['idUser'],
+                "nomUser" => $results[0]['nomUser'],
+                "prenomUser" => $results[0]['prenomUser'],
+                "ageUser" => $results[0]['ageUser'],
+                "adresseMailUser" => $results[0]['adresseMailUser']
+            ];
+            header("Location: logUser.php");
+        } 
+        else {
+        }
+    }
+
+    if(isset($_POST['deconnexion'])){
+       session_destroy();
+       header("Location: logUser.php"); 
+    }
+    ?>
+
+</body>
+
+</html>
