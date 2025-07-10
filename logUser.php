6faa10fabcd18cb08f886667a162d14f6ac6cd57
RomAFCI <adnot.rom@gmail.com>
2025-07-10 12:00:04 +0200

    save

--- logUser.php
+++ logUser.php
@@ -9,9 +9,9 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     // Active les erreurs PDO en exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie !";
-    echo "<br>";
-    echo "salut";
+    // echo "Connexion réussie !";
+    // echo "<br>";
+    // echo "salut";
 } catch (PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
 }
@@ -28,10 +28,10 @@ try {
 
 <body>
 
-<?php
+    <?php
 
-if(!isset($_SESSION['user'])){
-    echo '<form method="POST">
+    if (!isset($_SESSION['user'])) {
+        echo '<form method="POST">
         <label>Identifiant</label>
         <input type="text" name="identifiant">
         <label>Password</label>
@@ -39,20 +39,19 @@ if(!isset($_SESSION['user'])){
         <input type="submit" name="submitConnection" value="Se connecter">
 
     </form>';
-}
-else{
-echo'<form method="POST">
+    } else {
+        echo '<form method="POST">
     <input type="submit" name="deconnexion" value="Deconnexion">
 </form>';
 
-echo "Bonjour, " . $_SESSION['user']['nomUser'] . " " . $_SESSION['user']['prenomUser'] . ". Vous êtes connecté.";
-}
+        echo "Bonjour, " . $_SESSION['user']['nomUser'] . " " . $_SESSION['user']['prenomUser'] . ". Vous êtes connecté.";
+    }
 
-?>
+    ?>
 
- 
+
     <?php
-    if (isset($_POST['submitConnection'])) {
+    if (isset($_POST['submitConnection']) && !empty($_POST['identifiant']) && !empty($_POST['password'])) {
         $id = $_POST['identifiant'];
         $password = $_POST['password'];
 
@@ -61,25 +60,29 @@ echo "Bonjour, " . $_SESSION['user']['nomUser'] . " " . $_SESSION['user']['preno
         $stmt->execute();
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        var_dump($results);
+      
 
-        if ($password == $results[0]['passwordUser']) {
-            $_SESSION['user'] = [
-                "idUser" => $results[0]['idUser'],
-                "nomUser" => $results[0]['nomUser'],
-                "prenomUser" => $results[0]['prenomUser'],
-                "ageUser" => $results[0]['ageUser'],
-                "adresseMailUser" => $results[0]['adresseMailUser']
-            ];
-            header("Location: logUser.php");
+        if (isset($results[0]["passwordUser"])) {
+
+            if ($password == $results[0]['passwordUser']) {
+                $_SESSION['user'] = [
+                    "idUser" => $results[0]['idUser'],
+                    "nomUser" => $results[0]['nomUser'],
+                    "prenomUser" => $results[0]['prenomUser'],
+                    "ageUser" => $results[0]['ageUser'],
+                    "adresseMailUser" => $results[0]['adresseMailUser']
+                ];
+                header("Location: logUser.php");
+            }
         } 
         else {
+            echo "mot de passe incorrect";
         }
     }
 
-    if(isset($_POST['deconnexion'])){
-       session_destroy();
-       header("Location: logUser.php"); 
+    if (isset($_POST['deconnexion'])) {
+        session_destroy();
+        header("Location: logUser.php");
     }
     ?>
 
